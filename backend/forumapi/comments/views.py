296b7a8497de75cab20b3f6f09d24ccb742cd1eb
Tomaskkq <78925228+Tomaskkq@users.noreply.github.com>
2026-01-30 23:20:01 +0200

    Add users app Add serializers Add urls Add views

--- backend/forumapi/comments/views.py
+++ backend/forumapi/comments/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Comment
+from .serializers import CommentSerializer
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
+
+class CommentListCreateView(generics.ListCreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+class CommentDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticated]
--- backend/forumapi/posts/views.py
+++ backend/forumapi/posts/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Post
+from .serializers import PostSerializer
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
+
+class PostListCreateView(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated]
